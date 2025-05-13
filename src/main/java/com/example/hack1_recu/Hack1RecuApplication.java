807914c95ee0cd1e565fa9d8a3c000af3d5ca7b6
Yulinio Zavala <yulinio.zavala@utec.edu.pe>
2025-05-13 16:37:32 -0500

    Reposistorio de recuperación de hack 1

---
+++ src/main/java/com/example/hack1_recu/Hack1RecuApplication.java
@@ -0,0 +1,13 @@
+package com.example.hack1_recu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack1RecuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hack1RecuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hack1_recu/Hack1RecuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hack1_recu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack1RecuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
